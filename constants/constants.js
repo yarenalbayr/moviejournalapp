9e42abae7da9b2f6ad5f2962cd2146ae9bf898ea
Yaren Albayrak <yarenalbayr@gmail.com>
2024-04-22 00:47:32 +0300

    home and movie screen implementation

---
+++ constants/constants.js
@@ -0,0 +1,2 @@
+export const apiKey = "9b705e7bb7cca4103a93cf55c42780dd"
+
---
+++ navigation/appNavigation.jsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screen/HomeScreen';
+import MovieScreen from '../screen/MovieScreen';
+import PersonScreen from '../screen/PersonScreen';
+import SearchScreen from '../screen/SearchScreen';
+
+
+const Stack = createNativeStackNavigator(); 
+
+const AppNavigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home"  options={{headerShown: false }} component={HomeScreen} />
+                <Stack.Screen name="Movie" options={{headerShown: false}} component={MovieScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default AppNavigation;
